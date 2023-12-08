a67a0a1543ada93a4060f8ad073d89380d82d89a
JuliaBel5 <juliabel5@gmail.com>
2023-12-08 16:28:45 +0300

    feat: add project setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,16 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+const {
+    configure,
+    presets
+} = require("eslint-kit");
+
+module.exports = configure({
+    allowDebug: process.env.NODE_ENV !== "production",
+
+    presets: [
+        presets.imports(),
+        presets.node(),
+        presets.prettier(),
+        presets.typescript(),
+        presets.react()
+    ]
+});
